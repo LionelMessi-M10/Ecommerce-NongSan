b9cf3a3baf3313a7d710874c3e5f8ae126132b1d
huytagama <luongthanhhuyptit@gmail.com>
2025-11-09 23:15:49 +0700

    Update categoty api

--- src/main/java/com/multishop/controller/shop/CategoryController.java
+++ src/main/java/com/multishop/controller/shop/CategoryController.java
@@ -1,26 +1,16 @@
 package com.multishop.controller.shop;
 
-import org.springframework.data.domain.Page;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.multishop.model.dto.CategorySearchCriteria;
 import com.multishop.model.request.CategoryRequest;
 import com.multishop.model.response.CategoryResponse;
 import com.multishop.payload.ApiResponse;
 import com.multishop.service.CategoryService;
-
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -30,7 +20,7 @@ public class CategoryController {
 	private final CategoryService categoryService;
 
 	@GetMapping("/categories")
-	public ResponseEntity<?> getAllCategories(@ModelAttribute CategorySearchCriteria categorySearchCriteria) {
+	public ResponseEntity<?> getAllCategories(@RequestBody(required = false) CategorySearchCriteria categorySearchCriteria) {
 
 		Page<CategoryResponse> result = categoryService.getAll(categorySearchCriteria);
 
@@ -62,13 +52,13 @@ public class CategoryController {
 	}
 
 	@PostMapping("/categories")
-	public ResponseEntity<?> createCategory(@Valid @RequestBody CategoryRequest categoryRequest) {
+	public ResponseEntity<?> createCategory(@Valid @RequestBody(required = false) CategoryRequest categoryRequest) {
 		CategoryResponse newCategory = categoryService.create(categoryRequest);
 		return ResponseEntity.ok(ApiResponse.success(HttpStatus.CREATED, "Create category successfully", newCategory));
 	}
 
 	@PutMapping("/categories/{id}")
-	public ResponseEntity<?> updateCategory(@PathVariable Long id, @Valid @RequestBody CategoryRequest categoryRequest) {
+	public ResponseEntity<?> updateCategory(@PathVariable Long id, @Valid @RequestBody(required = false) CategoryRequest categoryRequest) {
 		CategoryResponse updateCategory = categoryService.update(id, categoryRequest);
 		return ResponseEntity.ok(ApiResponse.success(HttpStatus.OK, "Update category successfully", updateCategory));
 	}
